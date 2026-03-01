b16fe551eb005f61ada2e7ebf1b1ed0337b301fc
Eslamsaber28 <sabereslam819@gmail.com>
2026-03-01 17:36:43 +0200

    Fix inventory routes loading

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,37 +14,27 @@ use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
 use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        commands: __DIR__ . '/../routes/console.php',
-        health: '/up',
-        then: function () {
+   ->withRouting(
+    commands: __DIR__ . '/../routes/console.php',
+    health: '/up',
+    then: function () {
+        Route::middleware('web')->group(function () {
+            require base_path('routes/web.php');
 
-            // ===== Web Routes =====
-            Route::middleware('web')->group(function () {
+            foreach (glob(base_path('routes/web/*.php')) as $file) {
+                require $file;
+            }
+        });
 
-                // Load main web.php file
-                require base_path('routes/web.php');
+        Route::prefix('api')->middleware('api')->group(function () {
+            require base_path('routes/api.php');
 
-                // Load extra web routes in routes/web/*.php
-                foreach (glob(base_path('routes/web/*.php')) as $file) {
-                    require $file;
-                }
-            });
-
-            // ===== API Routes =====
-            Route::prefix('api')->middleware('api')->group(function () {
-
-                // Load main api.php file
-                require base_path('routes/api.php');
-
-                // Load extra api routes in routes/api/*.php if you have them
-                foreach (glob(base_path('routes/api/*.php')) as $file) {
-                    require $file;
-                }
-            });
-        }
-    )
+            foreach (glob(base_path('routes/api/*.php')) as $file) {
+                require $file;
+            }
+        });
+    }
+)
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->redirectGuestsTo(function (Request $request): string {
             $locale = $request->segment(1);
